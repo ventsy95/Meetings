d3b390e9ca0a0ec1358030e20796ae4a9c0bfdbe
Ventsy Mihalov <ventsy95@mail.bg>
2019-01-03 16:44:01 -0600

    Session changes + insertion validation changes.

--- src/main/java/com/meetings/conferent/service/MeetingService.java
+++ src/main/java/com/meetings/conferent/service/MeetingService.java
@@ -1,6 +1,7 @@
 package com.meetings.conferent.service;
 
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -31,7 +32,7 @@ public class MeetingService {
 
 	public boolean insert(Meeting meeting) {
 		meetingDao.openCurrentSessionWithTransaction();
-		if (!isTimeslotTaken(meeting)) {
+		if (!isTimeslotTaken(meeting) && isSameDay(toCalendar(meeting.getStartDate()), toCalendar(meeting.getEndDate()))) {
 			meetingDao.addMeeting(meeting);
 			meetingDao.closeCurrentSessionWithTransaction();
 			return true;
@@ -48,16 +49,16 @@ public class MeetingService {
 	}
 
 	public void delete(long id) {
-		Meeting entity = findById(id);
 		meetingDao.openCurrentSessionWithTransaction();
+		Meeting entity = meetingDao.findMeetingById(id);
 		meetingDao.deleteMeeting(entity);
 		meetingDao.closeCurrentSessionWithTransaction();
 	}
 
 	public List<Meeting> getMeetingsForRoom(long roomId) {
-		meetingDao.openCurrentSession();
+		meetingDao.openCurrentSessionWithTransaction();
 		List<Meeting> meetings = meetingDao.getMeetingsForRoom(roomId);
-		meetingDao.closeCurrentSession();
+		meetingDao.closeCurrentSessionWithTransaction();
 		return meetings;
 	}
 
@@ -66,7 +67,7 @@ public class MeetingService {
 		if (meeting.getStartDate() != null && meeting.getEndDate() != null) {
 			for (Meeting existMeeting : currentMeetings) {
 				if (existMeeting.getStartDate() != null && existMeeting.getEndDate() != null
-						&& meeting.getMeetingId() != existMeeting.getMeetingId()) {
+						&& meeting.getMeetingId() != existMeeting.getMeetingId() && isSameDay(toCalendar(meeting.getStartDate()), toCalendar(existMeeting.getStartDate()))) {
 					if (meeting.getStartDate().getTime() >= existMeeting.getStartDate().getTime()
 							&& meeting.getStartDate().getTime() < existMeeting.getEndDate().getTime()) {
 						return true;
@@ -83,5 +84,18 @@ public class MeetingService {
 			}
 		}
 		return false;
+	}
+	
+	private boolean isSameDay(Calendar date1, Calendar date2) {
+		if(date1.get(Calendar.DAY_OF_MONTH) == date2.get(Calendar.DAY_OF_MONTH) && date1.get(Calendar.MONTH) == date2.get(Calendar.MONTH) && date1.get(Calendar.YEAR) == date2.get(Calendar.YEAR)) {
+			return true;
+		}
+		return false;
+	}
+	
+	private Calendar toCalendar(Date date){ 
+		  Calendar cal = Calendar.getInstance();
+		  cal.setTime(date);
+		  return cal;
 	}
 }
